2f859ba404c3c1484aa6e6e920faea7e043b4608
Kingsley Owadara <kingsleyowadara@gmail.com>
2026-07-01 15:52:58 +0100

    Fix MainActivity package to dev.thetechforge.lintel, bump to 1.0.0+24

--- android/app/src/main/kotlin/dev/thetechforge/lintel/MainActivity.kt
+++ android/app/src/main/kotlin/dev/thetechforge/lintel/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.lintel
+package dev.thetechforge.lintel
 
 import io.flutter.embedding.android.FlutterActivity
 
--- android/app/src/main/kotlin/org/getlintel/app/lintel/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package org.getlintel.app.lintel
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
